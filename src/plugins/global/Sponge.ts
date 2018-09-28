3ff0240ce9ed999213c5c10c0d7543100a5332ac
Rich Searle <richard.searle@spongeuk.com>
2018-09-28 10:45:16 +0100

    some helpful debug tools (dat.GUI)

--- src/plugins/global/Sponge.ts
+++ src/plugins/global/Sponge.ts
@@ -2,6 +2,7 @@ import GameData from "./GameData";
 import HTMLUtils from "./HTMLUtils";
 import ScaleManager from "./ScaleManager";
 import Utils from "../utils/Utils";
+import * as dat from 'dat.gui';
 
 export default class Sponge extends Phaser.Plugins.BasePlugin {
   /**
@@ -11,6 +12,9 @@ export default class Sponge extends Phaser.Plugins.BasePlugin {
    */
   constructor(pluginManager: Phaser.Plugins.PluginManager) {
     super(pluginManager);
+
+    this.game = pluginManager.game;
+
     console.log("Sponge::constructor");
 
     // we might need this in the boot / controller class.
@@ -29,6 +33,25 @@ export default class Sponge extends Phaser.Plugins.BasePlugin {
       scale: this.game.config.zoom
     });
     this.scale = pluginManager.get("_scale") as ScaleManager;
+
+
+    if (this.data.getDataFor("global.debugMode")) {
+      this.debugGUI = new dat.GUI();
+      this.debugGUI.add(this, "restart_scene");
+    } 
+
+
+  }
+
+  /**
+   * placeholder for some common debug options (restarting scene etc.)
+   *
+   * @memberof Sponge
+   */
+  public restart_scene() {
+    console.log("restart scene");
+    // this.game.scene
+    
   }
 
   public boot() {
@@ -40,6 +63,9 @@ export default class Sponge extends Phaser.Plugins.BasePlugin {
             on a lot of the settings from json files which are now availible.
             ------------------------------------------------------ */
   }
+
+
+  public game: Phaser.Game;
 
   /**
    * access to data and tracking functions
@@ -71,4 +97,15 @@ export default class Sponge extends Phaser.Plugins.BasePlugin {
    * @memberof Sponge
    */
   public utils: Utils;
+  
+
+  /**
+   * a reference to dat.GUI - TODO: conditional inclusion only on debug mode.
+   *
+   * @type {(dat.GUI | null)}
+   * @memberof Sponge
+   */
+  public debugGUI: dat.GUI | null = null;
+
+  public test: boolean = false;
 }
