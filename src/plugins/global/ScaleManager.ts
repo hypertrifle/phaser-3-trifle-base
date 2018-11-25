153643a58f0a8f984d8b270931c8885916fe9c92
U-DESKTOP-0GHCVNG\rich <ricardo.searle@gmail.com>
2018-11-25 14:43:26 +0000

    fixed scaling and flixed game to bottom of screen.

--- src/plugins/global/ScaleManager.ts
+++ src/plugins/global/ScaleManager.ts
@@ -276,8 +276,8 @@ export default class ScaleManager extends Phaser.Plugins.BasePlugin {
 
 
     let styleString =
-      "-ms-transform-origin: top center; -webkit-transform-origin: top center;" +
-      " -moz-transform-origin: top center; -o-transform-origin: top center; transform-origin: top center;" +
+      "-ms-transform-origin: bottom center; -webkit-transform-origin: bottom center;" +
+      " -moz-transform-origin: bottom center; -o-transform-origin: bottom center; transform-origin: bottom center;" +
       " -ms-transform: scale(" +
       requiredScaling +
       "); -webkit-transform: scale3d(" +
