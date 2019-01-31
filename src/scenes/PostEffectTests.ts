5cbc5d8dcc1d0e008481c6fb0329219c19676015
Rich Searle <richard.searle@spongeuk.com>
2019-01-31 16:55:21 +0000

    formatting of post text effects

--- src/scenes/PostEffectTests.ts
+++ src/scenes/PostEffectTests.ts
@@ -26,7 +26,7 @@ export default class PostEffectTestsScene extends BaseScene {
     console.log("PostEffectTestsScene::preload");
 
 
-    
+
     this.load.bitmapFont('cast-iron', 'assets/fonts/cast-iron_0.png', 'assets/fonts/cast-iron.fnt');
 
     this.load.bitmapFont('lot', 'assets/fonts/lot_0.png', 'assets/fonts/lot.fnt');
@@ -41,12 +41,12 @@ export default class PostEffectTestsScene extends BaseScene {
 
     // create our background shader pipline.
     let postShader: SheneEffect = new SheneEffect(this.game, "glint", {
-      speed:1.5,
-      size:0.3, 
-      delay:3,
-      colour: {r:255,g:255,b:255}
+      speed: 1.5,
+      size: 0.3,
+      delay: 3,
+      colour: {r: 255,g: 255,b: 255}
     });
-    let postDebug = this.debugGUI.addFolder("Glint")
+    let postDebug = this.debugGUI.addFolder("Glint");
     postDebug.add(postShader,"size",0,0.2);
     postDebug.add(postShader,"speed",0,5);
     postDebug.add(postShader,"delay",0,10);
@@ -71,9 +71,9 @@ export default class PostEffectTestsScene extends BaseScene {
   initDebugTools() {
     this.debugGUI = new GUI();
   }
-  
-  testFont:Phaser.GameObjects.BitmapText;
-  testFont2:Phaser.GameObjects.BitmapText;
+
+  testFont: Phaser.GameObjects.BitmapText;
+  testFont2: Phaser.GameObjects.BitmapText;
 
   create() {
     super.create();
@@ -81,11 +81,11 @@ export default class PostEffectTestsScene extends BaseScene {
     this.initDebugTools();
     this.initShader();
 
-    this.testFont = this.add.dynamicBitmapText(this.dimensions.x/2,30,"lot","SCOOP", 128);
+    this.testFont = this.add.dynamicBitmapText(this.dimensions.x / 2,30,"lot","SCOOP", 128);
     this.testFont.setOrigin(0.5, 0);
     this.testFont.setPipeline("text");
     this.text = "hypertrifle";
-    
+
 
     let textDebug = this.debugGUI.addFolder("Text");
     textDebug.add(this,"text",0,0.2);
@@ -100,9 +100,9 @@ export default class PostEffectTestsScene extends BaseScene {
     console.log("SCOOP", this.testFont.getTextBounds());
     // console.log("LONGER STRING", this.testFont2.getTextBounds());
 
-    let testSprite = this.add.image(this.dimensions.x / 2, this.dimensions.y -10, "atlas.png", "test-sprite-2.png");
-    testSprite.setScale(1);   
-    testSprite.setOrigin(0.5,1);   
+    let testSprite = this.add.image(this.dimensions.x / 2, this.dimensions.y - 10, "atlas.png", "test-sprite-2.png");
+    testSprite.setScale(1);
+    testSprite.setOrigin(0.5,1);
     testSprite.setPipeline("glint");
 
 
@@ -116,20 +116,20 @@ export default class PostEffectTestsScene extends BaseScene {
     // this.input.on('pointermove', this.moveText,this);
   }
 
-  onLetterCallback(wut:any){
+  onLetterCallback(wut: any) {
     return wut;
   }
 
-  set text(text:string){
+  set text(text: string) {
     this.testFont.text = text;
-    let bounds:BitmapTextSize = this.testFont.getTextBounds();
+    let bounds: BitmapTextSize = this.testFont.getTextBounds();
 
     this.testFont.pipeline.setFloat2('offset',bounds.global.x,bounds.global.y);
     this.testFont.pipeline.setFloat2('size',bounds.global.width,bounds.global.height);
 
   }
 
-  get text():string{
+  get text(): string {
     return  "";
   }
 
@@ -145,9 +145,9 @@ export default class PostEffectTestsScene extends BaseScene {
 
     }
   }
-  moveText(pointer:PointerEvent){
+  moveText(pointer: PointerEvent) {
 
-    if(this.testFont && pointer){
+    if (this.testFont && pointer) {
       this.testFont.x = pointer.x;
       this.testFont.y = pointer.y;
     }
