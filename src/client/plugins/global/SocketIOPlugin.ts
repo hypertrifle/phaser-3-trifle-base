c844eb38e1a6221fe27b354ab8f240e0f36fba63
U-DESKTOP-0GHCVNG\rich <ricardo.searle@gmail.com>
2019-03-25 08:35:31 +0000

    MP mvp

--- src/client/plugins/global/SocketIOPlugin.ts
+++ src/client/plugins/global/SocketIOPlugin.ts
@@ -1,4 +1,10 @@
 import { Scene } from "phaser";
+import * as io from 'socket.io-client';
+
+export interface INetworkConnectionSettings {
+  host: string,
+
+}
 
 export enum ClientDestination {
   NONE= undefined,
@@ -6,12 +12,11 @@ export enum ClientDestination {
   ROOM,
   OTHERS_IN_ROOM,
   ALL
-
 }
 
 export enum RequestState {
   NONE = undefined,
-  REQUESTED = 0,
+  PENDING = 0,
   SENT,
   COMPLETE,
   FAILED,
@@ -19,23 +24,58 @@ export enum RequestState {
 }
 
 export interface IRequestObject {
-  action:"string",
-  packact:any;
+  action:string,
+  packact?:any; //should be typed depending on action
+  respose?:any; //again should be typed depending on action
   destination:ClientDestination,
-  onComplete:(request?:IRequestObject)=>void;
-  onFailed:(request?:IRequestObject)=>void;
+  onComplete?:(request?:IRequestObject)=>void;
+  onFailed?:(request?:IRequestObject)=>void;
   state?:RequestState;
+  //todo:Mutantions?
+}
+
+export interface IGameState {
+  uniqueID:string,
+  data:any; //cast per game, but this is maybe the easiest way to do it.
+}
+
+export class TestState implements IGameState {
+  uniqueID: "DemoGameState"
+  data: {
+    players:[],
+    options: {
+      death:number;
+    }
+  }
 }
 
 
 export default class SocketIOPlugin extends Phaser.Plugins.BasePlugin {
+
+
+  private _requestQueue:IRequestObject[];
+  private _socket:SocketIOClient;
+  public state:TestState = new TestState(); // todo: doesn't belong in this class, controller needs to be agnostic to the plugin.
+
    
   constructor(pluginManager: Phaser.Plugins.PluginManager) {
     super(pluginManager);
     this.game = pluginManager.game;
-
-
     console.log("SocketIOPlugin::constructor");
+  }
+
+  connect(conf:INetworkConnectionSettings){
+    console.log("AttemptinToConnectWithDetail::", conf)
+  }
+
+  requestAndSyncState(){
+    this.makeRequest({
+      action:"state-request",
+      destination: ClientDestination.ME,
+    onCOmplete: () = > {
+      this.state.data = 
+    }
+    })
   }
 
   makeRequest(requestObject:IRequestObject){
